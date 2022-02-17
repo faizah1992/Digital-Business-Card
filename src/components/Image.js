fc2729f9e39745aead6725e61eb9e455faa7c0e7
Faizah Ahsan <faizah1992@gmail.com>
2022-02-17 10:18:44 -0600

    files added

---
+++ src/components/Image.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+
+export default function Image(){
+
+    return(
+        <div>
+            <img src="../images/card-image.png" alt="Card image"/>
+        </div>
+    )
+}
